6003c661276487430ae5a022013cd4f0ecad1d37
Chimwemwe Masona <chimmasona@gmail.com>
2022-11-05 13:31:51 +0200

    added styling to the form

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -7,11 +7,13 @@ function Contact() {
       <div className="form-main">
         <div className="form">
           <form>
-            <h2>Contact Me</h2>
-            <p>Hi there, contact me to ask about anything you have in mind</p>
+            <div className="contact-heading">
+              <h2>Contact Me</h2>
+              <p>Hi there, contact me to ask about anything you have in mind</p>
+            </div>
 
             <div className="first-last">
-              <div>
+              <div className="name">
                 <label for="fname">First name</label>
                 <br />
                 <input
@@ -22,7 +24,7 @@ function Contact() {
                 <br />
               </div>
 
-              <div>
+              <div className="name">
                 <label for="lname">Last name</label>
                 <br />
                 <input
@@ -34,22 +36,43 @@ function Contact() {
               </div>
             </div>
 
-            <label for="email">Email</label>
-            <br />
-            <input
-              type="text"
-              id="email"
-              placeholder="yourname@email.com"
-            ></input>
-            <br />
+            <div className="email">
+              <label for="email">Email</label>
+              <br />
+              <input
+                type="text"
+                id="email"
+                placeholder="yourname@email.com"
+              ></input>
+              <br />
+            </div>
 
-            <label for="message">Message</label><br/>
-            <textarea id="message" name="textarea" rows="6" cols="50" placeholder="Send me a message and I'll reply you as soon as possible"></textarea><br/>
+            <div className="message">
+              <label for="message">Message</label>
+              <br />
+              <textarea
+                id="message"
+                name="textarea"
+                rows="6"
+                cols="50"
+                placeholder="Send me a message and I'll reply you as soon as possible..."
+              ></textarea>
+              <br />
+            </div>
 
-            <input type="checkbox" name="agreed" value="agree"></input>
-            <label for="agreed">You agree to providing your data to (name) who may contact you.</label><br/>
+            <div className="check">
+              <span className="checkbox">
+                <input type="checkbox" name="agreed" value="agree"></input>
+              </span>
+              <label for="agreed">
+                You agree to providing your data to (name) who may contact you.
+              </label>
+              <br />
+            </div>
 
-            <input type="submit" value="submit"></input>
+            <div className="sub-button">
+              <input type="submit" value="Send message"></input>
+            </div>
           </form>
         </div>
       </div>
